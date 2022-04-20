da5fd1385c192d1dd35d8f95bcc57e1203a56995
HatemGr <hatemgribi@gmail.com>
2022-04-20 17:07:04 +0200

    Adding DarkTrader

---
+++ Scrapping_project/lib/dark_trader.rb
@@ -0,0 +1,44 @@
+require 'open-uri'
+require 'nokogiri'
+
+
+def scrap(url)
+  return Nokogiri::HTML(URI.open(url))
+end
+
+page = scrap('https://coinmarketcap.com/all/views/all/')
+
+def fetch_currencies(noko_page)
+  currencies = []
+  noko_page.xpath("//tbody//a[@class='cmc-table__column-name--symbol cmc-link']").each do |currency| 
+    currencies << currency.text
+  end
+  return currencies
+end
+
+def fetch_prices(noko_page)
+  prices = []
+  noko_page.xpath("//tbody//td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price']//a[@class='cmc-link']").each do |price| 
+    prices << price.text
+  end
+  return prices
+end
+
+crypto_hash = Hash[fetch_currencies(page).zip(fetch_prices(page))]
+#puts crypto_hash
+
+def dark_trader(page)
+  currencies = fetch_currencies(page)
+  prices = fetch_prices(page)
+  currency_price_list = []
+  currencies.each.with_index {|currency,i| currency_price_list << {currency => prices[i]}}
+
+  return currency_price_list
+end
+
+puts dark_trader(page)
+
+# Pour LTC
+# //*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[21]/td[3]/div
+# Pour ATOM
+# //*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[22]/td[3]/div
---
+++ Scrapping_project/lib/tempCodeRunnerFile.rb
@@ -0,0 +1,1 @@
+cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right 
